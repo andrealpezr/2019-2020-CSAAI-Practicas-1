1185bcf86b75181c67dcb449addda1bfbcdcafda
Carmen Gonzalez Lopez <carmengl@f-l3210-pc06.aulas.etsit.urjc.es>
2020-02-27 10:43:04 +0100

    calculadora

--- P2/calculadora.js
+++ P2/calculadora.js
@@ -1,26 +1,37 @@
 console.log("Ejecutando JS...");
 
   var display = document.getElementById("display");
-  //no hace falta poner var, va a funcionar igual
-  var cero = document.getElementById("0");
-  var uno = document.getElementById("1");
-  var dos = document.getElementById("2");
-  var tres = document.getElementById("3");
-  var cuatro = document.getElementById("4");
-  var cinco = document.getElementById("5");
-  var seis = document.getElementById ("6");
-  var siete = document.getElementById ("7");
-  var ocho = document.getElementById ("8");
-  var nueve = document.getElementById("9");
-
   var igual = document.getElementById("igual");
   var coma = document.getElementById("coma");
-  var ac = document.getElementById("AC");
-  var reset = document.getElementById("reset");
+  var reset = document.getElementById("AC");
+  var delete = document.getElementById("delete");
   var suma = document.getElementById("suma");
   var resta = document.getElementById("resta");
   var multiplicacion = document.getElementById("multiplicacion");
   var division = document.getElementById("division");
-  uno.onclik = function(e) {
-    resultado.textContent = resultado.textContent + "1";
+
+  //no hace falta poner var, va a funcionar igual
+  //Variable let
+  let numeros = document.getElementsByClassName("numero");
+  for(i=0; i<numeros.length; i++){
+    numeros[i].onclick =(ev) =>{
+    // que es tarjet??????
+    numero(ev.target);
+    }
+  }
+
+  function digito(boton){
+    if(display.innerHTML=="0") {
+      display.innerHTML = boton.value;
+    }else{
+      display.innerHTML += boton.value;
+    }
+  }
+
+  suma.onclick = () => {
+    display.innerHTML += "+";
+  }
+
+  igual.onclick = () => {
+    display.innerHTML = eval(display.innerHTML);
   }
