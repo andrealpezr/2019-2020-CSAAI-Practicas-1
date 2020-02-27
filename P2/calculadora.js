3591efc10d803d583cdc2d13385f673f01af1b00
Carmen Gonzalez Lopez <carmengl@f-l3210-pc06.aulas.etsit.urjc.es>
2020-02-27 10:10:59 +0100

    calculadora

--- P2/calculadora.js
+++ P2/calculadora.js
@@ -1,27 +1,26 @@
-function init() {
-  var resultado = document.getElementById('resultado');
+console.log("Ejecutando JS...");
 
-  var cero = document.getElementById('0');
-  var uno = document.getElementById('1');
-  var dos = document.getElementById('2');
-  var tres = document.getElementById('3');
-  var cuatro = document.getElementById('4');
-  var cinco = document.getElementById('5');
-  var seis = document.getElementById ('6');
-  var siete = document.getElementById ('7');
-  var ocho = document.getElementById ('8');
-  var nueve = document.getElementById('9');
+  var display = document.getElementById("display");
+  //no hace falta poner var, va a funcionar igual
+  var cero = document.getElementById("0");
+  var uno = document.getElementById("1");
+  var dos = document.getElementById("2");
+  var tres = document.getElementById("3");
+  var cuatro = document.getElementById("4");
+  var cinco = document.getElementById("5");
+  var seis = document.getElementById ("6");
+  var siete = document.getElementById ("7");
+  var ocho = document.getElementById ("8");
+  var nueve = document.getElementById("9");
 
-  var igual = document.getElementById('igual');
-  var coma = document.getElementById('coma');
-  var ac = document.getElementById('AC');
-  var reset = document.getElementById('reset');
-  var suma = document.getElementById('suma');
-  var resta = document.getElementById('resta');
-  var multiplicacion = document.getElementById('multiplicacion');
-  var division = document.getElementById('division');
+  var igual = document.getElementById("igual");
+  var coma = document.getElementById("coma");
+  var ac = document.getElementById("AC");
+  var reset = document.getElementById("reset");
+  var suma = document.getElementById("suma");
+  var resta = document.getElementById("resta");
+  var multiplicacion = document.getElementById("multiplicacion");
+  var division = document.getElementById("division");
   uno.onclik = function(e) {
     resultado.textContent = resultado.textContent + "1";
   }
-
-}
