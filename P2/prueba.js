b2755767ff017e6c743eb101ff0fa03d0353005a
unknown <carmengl98>
2020-03-11 17:40:34 +0100

    prueba

---
+++ P2/prueba.js
@@ -0,0 +1,14 @@
+const Pelicula ={
+  nombre: 'Harry',
+  id :01,
+  reproducir(){
+    return 'reproduciendo...harry'
+  }
+}
+console.log(Pelicula.reproducir());
+
+
+// suma = document.getElementById("suma")
+// resta = document.getElementById("resta")
+// multiplicacion = document.getElementById("multiplicacion")
+// division = document.getElementById("division")
