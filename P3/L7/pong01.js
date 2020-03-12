8bad5d7a52ae46691c9743312477241e2697c364
unknown <carmengl98>
2020-03-12 09:38:21 +0100

    p3

---
+++ P3/L7/pong01.js
@@ -0,0 +1,16 @@
+console.log("Ejecutando JS...");
+
+//-- Obtener el objeto canvas
+const canvas = document.getElementById("canvas");
+
+//-- Sus dimensiones las hemos fijado en el fichero
+//-- HTML. Las imprimimos en la consola
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
+
+// En la consola de javascript del navegador podemos hacer pruebas. Escribimos esto:
+// x = 1
+// `x=${x}, x incrementada:${x+1}`
+// Veremos que aparece la cadena:
+//
+// "x=1, x incrementada:2"
