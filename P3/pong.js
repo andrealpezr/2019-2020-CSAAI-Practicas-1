b74bbd60e70bb06117ae4a4ced85365e8037ecb1
unknown <carmengl98>
2020-03-14 10:51:44 +0100

    P3

---
+++ P3/pong.js
@@ -0,0 +1,17 @@
+// Reset: Llevar la bola a su posición inicial
+// Init: Inicializar la bola, pasándole como argumento el contexto del canvas
+// Draw: Dibujar la bola
+// Update: Actualizar el estado de la bola
+
+function main()
+{
+
+  //-- Crear el canvas
+  var canvas = document.getElementById('display')
+  canvas.width = 600;
+  canvas.height = 400;
+
+  //-- Obtener el contexto del canvas
+  var ctx = canvas.getContext("2d");
+
+}
