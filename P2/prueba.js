dadf2d478f6984c86c936a32a009f08063900f00
unknown <carmengl98>
2020-03-18 18:59:36 +0100

    p

--- P2/prueba.js
+++ P2/prueba.js
@@ -1,11 +1,47 @@
-const Pelicula ={
-  nombre: 'Harry',
-  id :01,
-  reproducir(){
-    return 'reproduciendo...harry'
-  }
+console.log( "Que comiencen los juegos de JavaScript" );
+//esto es un objeto
+
+var objeto = {
+  //atributos:valor
+  //atributos:funcion(){}
+
+  botones:{
+      por: document.getElementById('*'),
+      reset: document.getElementById('reset'),
+      igual: document.getElementById('='),
+      entre: document.getElementById('/'),
+      display: document.getElementById('display'),
+  },
+  num: function() {
+
+ },
 }
-console.log(Pelicula.reproducir());
+//Variable let
+let numeros = document.getElementsByClassName("numero");
+for(i=0; i<numeros.length; i++){
+  numeros[i].onclick = (ev) => {
+  if(objeto.botones.display.innerHTML=="0") {
+      objeto.botones.display.innerHTML = ev.value;
+  }else{
+      objeto.botones.display.innerHTML += ev.value;
+  }
+ }
+}
+
+
+
+
+
+
+
+// const Pelicula ={
+//   nombre: 'Harry',
+//   id :01,
+//   reproducir(){
+//     return 'reproduciendo...harry'
+//   }
+// }
+// console.log(Pelicula.reproducir());
 
 
 // suma = document.getElementById("suma")
