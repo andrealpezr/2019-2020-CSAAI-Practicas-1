428c142449c9017b8d47299d4705329b5c4f12b9
unknown <carmengl98>
2020-03-24 20:11:17 +0100

    pong

--- P3/pong.js
+++ P3/pong.js
@@ -10,14 +10,11 @@ console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
 
 const ctx = canvas.getContext("2d");
 
-//-- Definir los objetos sin function
 //-- Objeto: Bola
 const bola = {
 
-  //-- Constante: Tamaño de la bola
   size : 5,
 
-  //-- Contante: Posicion inicial de la bola
   x_ini : 100,
   y_ini : 200,
 
@@ -30,18 +27,16 @@ const bola = {
   vy_ini : 3,
 
   //-- Velocidad genérica de la bola
-  //-- Inicialmente a cero
   vx : 0,
   vy : 0,
 }
 
 //-- Objeto raqueta
 const raqI = {
-  //-- Constante: Tamaño de la raqueta
+
   width : 10,
   height: 40,
 
-  //-- Constante: Posicion inicial
   x_ini : 50,
   y_ini : 100,
 
@@ -55,49 +50,48 @@ const raqI = {
 //-- Pintar todos los objetos en el canvas
 function draw() {
 
-  //----- Dibujar la Bola
+  //---- Dibujar la Bola
   ctx.beginPath();
   ctx.fillStyle='white';
 
   //-- x,y, anchura, altura
-  ctx.rect(50, 200, 10, 10);
+  ctx.rect(bola.x, bola.y, bola.size, bola.size);
   ctx.fill();
 
-  //------- Dibujar las raquetas
+  //---- Dibujar las raquetas
   ctx.beginPath();
   ctx.fillStyle='white';
 
   //-- Raqueta izquierda
-  ctx.rect(50, 100, 10, 40);
+  ctx.rect(raqI.x, raqI.y, raqI.width, raqI.height);
 
   //-- Raqueta derecha
   ctx.rect(540, 300, 10, 40);
 
-  //-- Pintar!
+  //pintar
   ctx.fill();
 
-  //--------- Dibujar la red
+  //---- Dibujar la red
   ctx.beginPath();
 
-  //-- Estilo de la linea: discontinua
-  //-- Trazos de 10 pixeles, y 10 de separacion
   ctx.setLineDash([10, 10]);
   ctx.strokeStyle = 'white';
   ctx.lineWidth = 2;
-  //-- Punto superior de la linea. Su coordenada x está en la mitad
-  //-- del canvas
+  
+  //-- Punto superior de la linea. Su coordenada x está en la mitad del canvas
   ctx.moveTo(canvas.width/2, 0);
 
   //-- Dibujar hasta el punto inferior
   ctx.lineTo(canvas.width/2, canvas.height);
   ctx.stroke();
 
-  //------ Dibujar el tanteo
+  //---- Dibujar el tanteo
   ctx.font = "100px Arial";
   ctx.fillStyle = "white";
   ctx.fillText("0", 200, 80);
   ctx.fillText("1", 340, 80);
 }
+
 
 //-- Inicializa la bola: A su posicion inicial
 bola.x = bola.x_ini;
