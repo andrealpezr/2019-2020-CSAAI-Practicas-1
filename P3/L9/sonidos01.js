fea8554b0a2e78da4a7d771cbaf6ff8b5f1acb05
unknown <carmengl98>
2020-03-28 12:31:26 +0100

    cambios varios

--- P3/L9/sonidos01.js
+++ P3/L9/sonidos01.js
@@ -1,12 +1,12 @@
 console.log("Ejecutando JS...");
 
 //--Obtener los botones
-const click = document.getElementById("click")
+const click = document.getElementById("click");
 const what = document.getElementById("what");
 
 //-- Crear los elementos de sonido
-const click_sound = new Audio('click.mp3');
-const what_sound = new Audio('what.mp3');
+const click_sound = new Audio('file:///C:/Users/Carmen/github/2019-2020-CSAAI-Practicas/P3/click.mp3');
+const what_sound = new Audio('file:///C:/Users/Carmen/github/2019-2020-CSAAI-Practicas/P3/what.mp3');
 
 click.onclick = () => {
   click_sound.currentTime = 0;
