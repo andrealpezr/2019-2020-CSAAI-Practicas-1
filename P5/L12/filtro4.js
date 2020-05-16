19c780ff31bb8c21d26401433d7bc438f1e5c289
unknown <carmengl98>
2020-05-16 12:09:02 +0200

    Practicas

--- P5/L12/filtro4.js
+++ P5/L12/filtro4.js
@@ -11,15 +11,32 @@ const ctx = canvas.getContext('2d');
 //-- que esté totalmente cargada
 img.onload = function () {
 
-  console.log("Imagen cargada");
-
-  //-- Se establece como tamaño del canvas el mismo que el de la imagen original
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
   canvas.width = img.width;
   canvas.height = img.height;
 
   //-- Situar la imagen original en el canvas
   //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0);
+
+  //-- Obtener la imagen del canvas en pixeles
+  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  //-- Obtener el array con todos los píxeles
+  let data = imgData.data
+
+  //-- Eliminar el canal Rojo:recorrer el array de datos
+  //-- eliminado el canal rojo y dejando el resto igual que
+  //-- estaba
+  for (let i = 0; i < data.length; i+=4) {
+    data[i] = 0; //-- Canal rojo a 0
+  }
+
+  //-- Poner la imagen modificada en el canvas
+  ctx.putImageData(imgData, 0, 0);
+
+  console.log("hola....");
 };
 
 console.log("Fin...");
