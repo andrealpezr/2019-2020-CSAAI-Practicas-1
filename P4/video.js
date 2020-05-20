fc09de2f559754a43af4067362ab10771aab2aa2
unknown <carmengl98>
2020-05-20 18:52:30 +0200

    video

--- P4/video.js
+++ P4/video.js
@@ -8,10 +8,6 @@
 //dos segundos en bucle, constantemente. Mientras está activo ese modo se debe poder cambiar
 //la fuente de vídeo (y el bucle se hará con la nueva fuente)
 
-// Imagen estática: Añadir un cuarto canal que permita mostrar una imagen estática, con un
-//mensaje que ponga "Emisión en Pruebas". Este canal se podrá seleccionar en cualquier momento
-
-
 console.log("Ejecutando JS...");
 
 //----- Obtener elemento de video y configurarlo
@@ -21,11 +17,11 @@ const video3 = document.getElementById("video3")
 const emision = document.getElementById("emision")
 
 //-- Tamaño de la pantalla de video
-video1.width=400;
+video1.width=355;
 video1.height=200;
-video2.width=400;
+video2.width=355;
 video2.height=200;
-video3.width=400;
+video3.width=355;
 video3.height=200;
 emision.width=800;
 emision.height=400;
@@ -34,53 +30,131 @@ emision.height=400;
 video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
 video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4"
 video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
-emision.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
+emision.poster="test.jpg";
 
 //-- Obtener los botones
 const boton1 = document.getElementById("boton1")
 const boton2 = document.getElementById("boton2")
 const boton3 = document.getElementById("boton3")
+const bucle = document.getElementById("bucle")
+repeat = false;
+var init = 0;
+var final = 0;
 
-video1.play();
-video2.play();
-video3.play();
+//CON SONIDO
+video1.onmouseover = () => {
+  console.log("Mouse over video1!!");
+  video1.muted = false;
+}
+video2.onmouseover = () => {
+  console.log("Mouse over video2!!");
+  video2.muted = false;
+}
+video3.onmouseover = () => {
+  console.log("Mouse over video3!!");
+  video3.muted = false;
+}
+emision.onmouseover = () => {
+  console.log("Mouse over emision!!");
+  emision.muted = false;
+}
 
-//-- Función de retrollamada del botón de ver
+// SIN SONIDO
+video1.onmouseout = () => {
+  console.log("Mouse out video1!!");
+  video1.muted = true;
+}
+video2.onmouseout = () => {
+  console.log("Mouse out video2!!");
+  video2.muted = true;
+}
+video3.onmouseout = () => {
+  console.log("Mouse out video3!!");
+  video3.muted = true;
+}
+
+emision.onmouseout = () => {
+  console.log("Mouse out emision!!");
+  emision.muted = true;
+}
+
 boton1.onclick = () => {
-  console.log("Click! video 1");
   emision.src = video1.src;
+  emision.currentTime = video1.currentTime;
+  video1.style.border = "6px solid red";
+  video2.style.border = "6px solid white";
+  video3.style.border = "6px solid white";
   emision.play();
 };
 
 boton2.onclick = () => {
-  console.log("Click! video 2");
   emision.src = video2.src;
+  emision.currentTime = video2.currentTime;
+  video1.style.border = "6px solid white";
+  video2.style.border = "6px solid red";
+  video3.style.border = "6px solid white";
   emision.play();
 };
 
 boton3.onclick = () => {
-  console.log("Click! video 3");
   emision.src = video3.src;
+  emision.currentTime = video3.currentTime;
+  video1.style.border = "6px solid white";
+  video2.style.border = "6px solid white";
+  video3.style.border = "6px solid red";
   emision.play();
 };
 
-//-- Funcion de retrollamada del boton de parar
-pause1.onclick = () => {
-  video1.pause();
-  video1.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
-  emision.src=null;
-  video1.src=null;
+bucle.onclick = () => {
+  console.log(repeat);
+  init = emision.currentTime;
+  final = emision.currentTime + 2;
+  if(!repeat){
+    repeat = true;
+    console.log(repeat);
+  }else{
+    repeat = false;
+    console.log(repeat);
+  }
 }
-pause2.onclick = () => {
-  video2.pause();
-  video2.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
-  emision.src=null;
-  video2.src=null;
-}
-pause3.onclick = () => {
-  video3.pause();
-  video3.poster="https://github.com/myTeachingURJC/2019-2020-CSAAI/raw/master/L10/test.png";
-  emision.src=null;
-  //-- Quitar la fuente de video, para que se muestre la imagen definida en el atributo poster
-  video3.src=null;
- }
+
+setInterval(()=>{
+    console.log(init, final);
+    if (repeat){
+         if (emision.currentTime > final){
+           emision.currentTime = init;
+         }
+     }
+    //tiempo.innerHTML = hora(display.currentTime);
+  },20); //-- timer
+
+emision.addEventListener("timeupdate",function(ev){
+    vtime = emision.currentTime;
+    h = Math.floor( vtime / 3600 );
+    m = Math.floor( (vtime % 3600) / 60 );
+    s = Math.floor(vtime % 60);
+    time = h + ":" + m + ":" + s;
+    document.getElementById("crono").innerHTML = time;
+  },true);
+
+
+// function startTime() {
+//   var today = new Date();
+//   var h = today.getHours();
+//   var m = today.getMinutes();
+//   var s = today.getSeconds();
+//   var minuto = (today.getMinutes()<9)?"0"+today.getMinutes():today.getMinutes();
+//    var segundo = (today.getSeconds()<9)?"0"+today.getSeconds():today.getSeconds();
+//   console.log( h, minuto , segundo);
+//   // add a zero in front of numbers<10
+//   m = checkTime(m);
+//   s = checkTime(s);
+//   document.getElementById("crono").innerHTML = h + ":" + m + ":" + s;
+//   var t = setTimeout(function(){ startTime() }, 500);
+// }
+// function checkTime(i){
+//   if (i < 10) {
+//       i = "0" + i;
+//     }
+//     return i;
+// }
